49c60a9cc3bb8fb0f3d1cf9400bcafdc21d294c9
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 21:15:31 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,7 +24,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    output = Song.all.map {|song| song.name.sort!}
+    output = Song.all.map {|song| song.name}
     output.each_with_index{|item, index| puts "#{index + 1}. #{item}."}
   end
 end
