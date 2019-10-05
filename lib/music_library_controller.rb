22195f3cd16754d57c62a0456254aa9cca93142a
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 22:27:33 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -62,7 +62,7 @@ class MusicLibraryController
     input = gets.strip.to_i
     output = Song.all.uniq
     output = output.sort_by! {|song| song.name}
-    selection = output[4]
+    selection = output[3]
     #binding.pry
     puts "Playing #{selection.name}"
   end
