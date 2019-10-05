d7f28d8ba8354de7db7246dfaf0b667533083288
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 22:22:49 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -61,7 +61,7 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
     output = Song.all
-    output = output.uniq!.sort_by! {|song| song.name}
+    output = output.sort_by {|song| song.name}
     selection = output[input+1]
     binding.pry
     puts "Playing #{selection.name}"
