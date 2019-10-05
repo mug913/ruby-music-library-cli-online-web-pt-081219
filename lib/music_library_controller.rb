4c04b7866c361db3d45c9971f1f3217b79440d59
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 21:43:26 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -31,10 +31,8 @@ class MusicLibraryController
   
   def list_artists
     output = Artist.all.map {|item| item.name}
-    binding.pry
     output.uniq.sort!
     output.each_with_index{|item, index| puts "#{index + 1}. #{item}"}
-    binding.pry
   end
 end
 
