2e571980fa6c9a4b9aa27ff62437e41ebc2e0dc1
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 21:29:25 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -32,6 +32,6 @@ class MusicLibraryController
     def list_artists
     output = Song.all.each {|song| song}
     output.uniq!.sort_by! {|song| song.artist.name} 
-    output.each_with_index{|item, index| puts "#{index + 1}. #{item.artist.name} - #{item.name} - #{item.genre.name}"}
+    output.each_with_index{|item, index| puts "#{index + 1}. #{item.artist.name}"}
   end
 end
