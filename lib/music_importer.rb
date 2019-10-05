a9827daf81d4e9fba340d1baeaba32f40a91e29d
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 20:33:19 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -13,8 +13,8 @@ class MusicImporter
 
   def import 
     file_list = []
-    file_list <<  Dir.entries(path).reject {|file| File.directory?(file) || file[0].include?('.')}
-    file_list.map {|filename| Song.create_from_filename(filename)}
+    file_list <<  Dir.entries(path).each.reject {|file| File.directory?(file) || file[0].include?('.')}
+    file_list.each {|filename| Song.create_from_filename(filename)}
   end
     
     
