819cf30a55a2183e78b1fa7ed5bf680e4b8a9fa7
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 19:44:18 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,15 +1,17 @@
-module Concerns::Findable
+module Concerns
+  module Findable
    
-  def self.find_by_name(name)
-    target = @@all.select{|target| target.name == name}
-    target[0]
-  end
+    def self.find_by_name(name)
+      target = @@all.select{|target| target.name == name}
+      target[0]
+    end
   
-  def self.find_or_create_by_name(name)
-    if  self.find_by_name(name) == nil
-      self.class.create(name)
-    else 
-      self.find_by_name(name)
+    def self.find_or_create_by_name(name)
+      if  self.find_by_name(name) == nil
+        self.class.create(name)
+      else 
+        self.find_by_name(name)
+      end
     end
   end
 end
