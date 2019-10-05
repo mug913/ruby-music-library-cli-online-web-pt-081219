75f982d599c6c92e60aa8dfce256478e41b22b37
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 22:46:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -23,8 +23,17 @@ class MusicLibraryController
       case input
       when "list songs"
         self.list_songs
+      when "list artists"
+        self.list_artists 
+      when "list genres"
+        self.list_genres 
+      when "list artist"
+        self.list_songs_by_artist 
+      when "list genre" 
+        self.list_songs_by_genre 
+      when "play song"
+        self.play_song 
       end
-      
     end
   end
   
