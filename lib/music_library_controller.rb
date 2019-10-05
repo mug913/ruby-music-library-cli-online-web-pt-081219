f5038dead51d57f3e686757c2e8af21deecb664d
Brad Alexander <brad.alexander913@gmail.com>
2019-10-05 21:01:50 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,6 +24,7 @@ class MusicLibraryController
   end
   
   def list_songs
+    binding.pry
     output = Song.all.each {|song| song}
     output.sort!
     output.each_with_index{|item, index| puts "#{index + 1}. #{item}."}
